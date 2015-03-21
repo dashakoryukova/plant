85b3c6b049b1a896e33e48b3e9a55742e48fcb99
Daria Koryukova <dashakoryukova@gmail.com>
2015-03-21 20:13:18 +0300

    list plants fr om db

--- andr/app/src/main/java/com/webster/AddNewPlant.java
+++ andr/app/src/main/java/com/webster/AddNewPlant.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,8 +16,9 @@ import com.webster.model.Plant;
 /**
  * Created by Bublik on 10.03.2015.
  */
-public class AddNewPlant  extends Activity{
-private static int count=0;
+public class AddNewPlant extends Activity {
+    private static int count = 0;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addnewplant);
@@ -24,16 +26,17 @@ private static int count=0;
         addPlant.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText nameEditText = (EditText)findViewById(R.id.plantsName);
-                String name=nameEditText.getText().toString();
-                EditText descEditText = (EditText)findViewById(R.id.plantsDescription);
-                String desc=descEditText.getText().toString();
-                Plant plant = new Plant(count+"");
+                EditText nameEditText = (EditText) findViewById(R.id.plantsName);
+                String name = nameEditText.getText().toString();
+                EditText descEditText = (EditText) findViewById(R.id.plantsDescription);
+                String desc = descEditText.getText().toString();
+
+                Plant plant = new Plant(count + "");
                 plant.setName(name);
                 plant.setDescription(desc);
-                System.out.println(plant.getId()+" "+plant.getName()+" "+plant.getDescription());
+                Log.d("plant", plant.getId() + " " + plant.getName() + " " + plant.getDescription());
                 count++;
-                Intent intent = new Intent(getApplicationContext(), WebsterActivity.class);
+                Intent intent = new Intent(getApplicationContext(), Plant.class);
                 startActivity(intent);
             }
         });
